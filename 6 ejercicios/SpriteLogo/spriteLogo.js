5c7c214eb9d959727fc00f4c90bf996bd542c230
Alejandro <alejandrosantalucia@gmail.com>
2020-12-14 18:25:51 -0300

    animando sprites

---
+++ 6 ejercicios/SpriteLogo/spriteLogo.js
@@ -0,0 +1,10 @@
+const $logo = document.getElementById (`logo`)
+
+$logo.addEventListener (`click`, (event) =>{
+    // console.log(event)
+       $logo.classList.toggle(`is-click`);
+
+})
+
+
+// $logo.classList.toggle(`pulse`);
